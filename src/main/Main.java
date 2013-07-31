d47219fc04e10867c87d3f75ea0c7f4f403fc422
ImaPotato <Scott.a@outlook.com>
2013-07-31 21:57:01 +1200

    Just little updates whilst I get my head around this

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,24 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import board.Board;
+
 public class Main {
+	private static int players;
+	
+	public static void main(String[] args) {
+		System.out.println("Cluedo");
+		System.out.println("Please enter the number of players");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			players = Integer.parseInt(br.readLine());
+		} catch (Exception e) {
+			
+		}
+		System.out.println(players);
+		Board b = new Board();
+	}
 
 }
