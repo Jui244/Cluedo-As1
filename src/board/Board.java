b35d03b47d80a1b9ea4e764ae73cae8b08a4bf3e
ImaPotato <Scott.a@outlook.com>
2013-08-11 16:47:49 +1200

    Patch 2.0 The Road Warrior

--- src/board/Board.java
+++ src/board/Board.java
@@ -483,9 +483,9 @@ public class Board {
 			System.out.println();
 		}
 
-		System.out.println("    ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨");
-		System.out.println("    1 2 3 4 5 6 7 8 9 1 1 1 1 1 1 1 1 1 1 2 2 2 2");
-		System.out.println("                      0 1 2 3 4 5 6 7 8 9 0 1 2 3");
+		System.out.println("    ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨");
+		System.out.println("    0 1 2 3 4 5 6 7 8 9 1 1 1 1 1 1 1 1 1 1 2 2 2 2 2");
+		System.out.println("                        0 1 2 3 4 5 6 7 8 9 0 1 2 3 4");
 	}
 
 	public void init(){
